1a04c618e0b587774f172c5cbe3b9bb8e6d553fe
OrhunTokdemir <148989093+OrhunTokdemir@users.noreply.github.com>
2025-06-05 17:26:13 +0300

    .

--- bisectionMethod.c
+++ bisectionMethod.c
@@ -1,8 +1,24 @@
 #include<stdio.h>
 #include "common.h"
 
-void bisectionMethod(double a,double b, double hata){
 
+
+void bisectionMethod(double a,double b, double hata){
+    while(!checkRoot(a,b)){
+        printf("Verilen degerler arasinda kok yoktur. yeniden deger giriniz.\n");
+        printf("a:");
+        scanf("%lf",&a);
+        printf("\nb:");
+        scanf("%lf",&b);
+    }
+    double p0=(a+b)/2;
+    if(checkRoot(a,p0)){
+        b=p0;
+    }
+    else if(checkRoot(p0,b)){
+        a=p0;
+    }
+    
 }
 
 int main(){
--- common.h
+++ common.h
@@ -1,4 +1,8 @@
 #include<stdlib.h>
+double f(double x) {
+    return x * x - 4; 
+}
+
 int mutlakHata(double p1,double p0,double hata) {
     if(abs(p1-p0)<hata){
         return 1;
@@ -15,9 +19,16 @@ int bagilHata(double p1,double p0,double hata) {
     }
 }
 int checkRoot(double a, double b) {
-    if(a*b<0){
-        return 1; // Root exists
+    int pn = (a+b) / 2;
+    if (f(a) * f(b) < 0) {
+        return 1; // There is a root in the interval [a, b]
     } else {
-        return 0; // No root exists
-    }
+        return 0; // No root in the interval
+    }   
+}
+void goster(double a, double b, double mutlakHata) {
+    printf("a: %lf\n", a);
+    printf("pn")
+    printf("b: %lf\n", b);
+    printf("Hata: %lf\n", mutlakHata);
 }
